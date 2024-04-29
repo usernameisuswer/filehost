85046d4d93da47dff3b700e4cfe0d1b9efefd7ad
usernameisuswer <149395376+usernameisuswer@users.noreply.github.com>
2024-04-29 20:33:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
+import requests
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.types import ContentType
+from aiogram.utils import executor
 
-TG_BOT_TOKEN = "YOUR_BOT_TOKEN"
+TG_BOT_TOKEN = ""
 bot = Bot(token=TG_BOT_TOKEN)
 dp = Dispatcher(bot)
 
@@ -19,8 +21,11 @@ async def handle_document(message: types.Message):
     
     # Upload file to file host site
     files = {'file': open(file_info.file_unique_id + "_" + message.document.file_name, 'rb')}
-    response = requests.post("https://hikkabot.serv00.net/upload.php", files=files)
+    response = requests.post("https://hikkabot.serv00.net/script.php", files=files)
     
     os.remove(file_info.file_unique_id + "_" + message.document.file_name)
 
     await message.answer(f"File has been uploaded to file host site: {response.text}")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
